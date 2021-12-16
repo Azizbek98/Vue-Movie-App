ffe362a541cda6a93ead93b8f09b6fd33a7f48a0
Azizbek Samatov <azizbeksamatov98@gmail.com>
2021-12-16 12:08:12 +0500

    feat: adding some stylings to home component

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -2,8 +2,8 @@
   <div class="home">
     <div class="feature-card">
       <router-link to="/movie/tt1375666">
-        <img src="" alt="movie-image" />
-        <div class="detail">
+        <img src="" alt="movie-image" class="featured-img" />
+        <div class="details">
           <h3>Inception</h3>
           <p>Description here:</p>
         </div>
@@ -22,6 +22,19 @@ export default {};
 .home {
   .feature-card {
     position: relative;
+
+    .featured-img {
+      display: block;
+      widows: 100%;
+      height: 300px;
+      object-fit: cover;
+
+      position: relative;
+      z-index: 0;
+    }
+
+    .details {
+    }
   }
 }
 </style>
