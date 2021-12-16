dcb4f5c25048b9d32cd938d3bfe003e3e1bf85f2
Azizbek Samatov <azizbeksamatov98@gmail.com>
2021-12-16 20:52:17 +0500

    feat: adding logic of movie info component

--- src/views/MovieDetail.vue
+++ src/views/MovieDetail.vue
@@ -1,9 +1,34 @@
 <template>
-  <div class="movie-detail">Detail {{ $route.params.id }}</div>
+  <div class="movie-detail">
+    <h2>Movie title {{ $route.params.id }}</h2>
+  </div>
 </template>
 
 <script>
-export default {};
+import { ref, onBeforeMount } from "vue";
+import { useRoute } from "vue-router";
+import env from "@/env.js";
+
+export default {
+  setup() {
+    const movie = ref({});
+    const route = useRoute();
+
+    onBeforeMount(() => {
+      fetch(
+        `http://www.omdbapi.com/?apikey=${env.apikey}&i=${route.params.id}&plot=full`
+      )
+        .then((response) => response.json())
+        .then((data) => {});
+    });
+
+    return {
+      movie,
+    };
+  },
+};
+
+// Detail {{ $route.params.id }}
 </script>
 
 <style></style>
