3d854d7d40c3a4b4aa2b30e69b3c16972faf64bc
Azizbek Samatov <azizbeksamatov98@gmail.com>
2021-12-16 12:05:42 +0500

    feat: adding structure of home component

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,5 +1,15 @@
 <template>
-  <div class="home">Home</div>
+  <div class="home">
+    <div class="feature-card">
+      <router-link to="/movie/tt1375666">
+        <img src="" alt="movie-image" />
+        <div class="detail">
+          <h3>Inception</h3>
+          <p>Description here:</p>
+        </div>
+      </router-link>
+    </div>
+  </div>
 </template>
 
 <script>
@@ -7,3 +17,11 @@
 
 export default {};
 </script>
+
+<style lang="scss">
+.home {
+  .feature-card {
+    position: relative;
+  }
+}
+</style>
