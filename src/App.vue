91087a4ff2ccff1f006874761c2c07c2031c68f7
Azizbek Samatov <azizbeksamatov98@gmail.com>
2021-12-16 11:34:44 +0500

    feat: adding main structure of the application

--- src/App.vue
+++ src/App.vue
@@ -1,5 +1,26 @@
 <template>
-  <router-view />
+  <header>
+    <router-link to="/">
+      <h1><span>Vue</span> Movies</h1>
+    </router-link>
+  </header>
+  <main>
+    <router-view />
+  </main>
+  <footer>
+    <p>Footer</p>
+  </footer>
 </template>
 
-<style lang="scss"></style>
+<style lang="scss">
+* {
+  margin: 0;
+  padding: 0;
+  box-sizing: border-box;
+  font-family: "Fira Sans", sans-serif;
+
+  &::selection {
+    background-color: transparentize(#42b883, 0.5);
+  }
+}
+</style>
